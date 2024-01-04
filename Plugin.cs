e3b926a97ebcd5d8f5b8843354e4ed4a6183a7a2
tairasoul <mjrcoetzee+1@outlook.com>
2024-01-04 16:04:58 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace Speedometer
+{
+    [BepInPlugin("tairasoul.vaproxy.speedometer", "Speedometer", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool init = false;
+        void Awake()
+        {
+            Logger.LogInfo("Speedometer is awake.");
+        }
+
+        void Start() => Init();
+        void OnDestroy() => Init();
+
+        void Init()
+        {
+            if (init) return;
+            init = true;
+            GameObject Speedometer = new GameObject("Speedometer");
+            DontDestroyOnLoad(Speedometer);
+            Speedometer.AddComponent<Speedometer>();
+        }
+    }
+}
---
+++ Speedometer.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Speedometer
+{
+    internal class Speedometer : MonoBehaviour
+    {
+        Text speedText;
+
+        void Awake()
+        {
+            SceneManager.activeSceneChanged += SceneLoad;
+        }
+
+        internal void SceneLoad(Scene old, Scene newS)
+        {
+            if (newS.name != "Intro" && newS.name != "Menu")
+            {
+                GameObject Area = GameObject.Find("UI/ui/Area");
+                GameObject Pos = GameObject.Instantiate(Area);
+                Pos.name = "Speed";
+                Pos.transform.SetParent(Area.transform.parent);
+                Destroy(Pos.GetComponent<Locations>());
+                Pos.GetComponent<RectTransform>().anchoredPosition = new Vector2(-806.1818f, -550.7941f);
+                speedText = Pos.GetComponent<Text>();
+                speedText.alignment = TextAnchor.UpperLeft;
+            }
+        }
+
+        internal void Update()
+        {
+            if (speedText != null)
+            {
+                GameObject sen = GameObject.Find("S-105");
+                Rigidbody rigid = sen.GetComponent<Rigidbody>();
+                speedText.text = $"{Mathf.Round(rigid.velocity.x)} {Mathf.Round(rigid.velocity.y)} {Mathf.Round(rigid.velocity.z)}";
+            }
+        }
+    }
+}
